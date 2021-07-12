8ac5ae430849b1b976a4f5fde9459ac3b2e35290
Collin <collinhallett@gmail.com>
2021-07-12 03:16:06 -0500

    updated delete and edit functions to include new tasks array to store tasks in local storage next

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var pageContentEl = document.querySelector("#page-content");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
-
+var tasks = []; 
 var completeEditTask = function(taskName, taskType, taskId) {
   //find matching task list item
   var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
@@ -12,6 +12,14 @@ var completeEditTask = function(taskName, taskType, taskId) {
   //set new values
   taskSelected.querySelector("h3.task-name").textContent = taskName; 
   taskSelected.querySelector("span.task-type").textContent = taskType;
+
+  //loop through tasks array and task object with new content 
+  for (var i = 0; i < tasks.length; i++) {
+    if (tasks[i].id === parseInt(taskId)) {
+      tasks[i].name = taskName; 
+      tasks[i].type = taskType;
+    }
+  }
 
   alert("Task Updated");
   
@@ -44,7 +52,8 @@ var taskFormHandler = function (event) {
   else {
     var taskDataObj = {
       name: taskNameInput,
-      type: taskTypeInput
+      type: taskTypeInput,
+      status: "to do" 
     };
   
     createTaskEl(taskDataObj);
@@ -66,6 +75,9 @@ var createTaskEl = function(taskDataObj) {
   // add HTML content to div
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl);
+
+  taskDataObj.id = taskIdCounter;
+  tasks.push(taskDataObj);
 
   // add buttons to list item 
   var taskActionsEl = createTaskActions(taskIdCounter); 
@@ -182,6 +194,14 @@ var taskStatusChangeHandler = function(event) {
   else if (statusValue === "completed") {
     tasksCompletedEl.appendChild(taskSelected);
   }
+
+  // update task's in tasks array
+for (var i = 0; i < tasks.length; i++) {
+  if (tasks[i].id === parseInt(taskId)) {
+    tasks[i].status = statusValue;
+  }
+}
+console.log(tasks);
 }
 
 
